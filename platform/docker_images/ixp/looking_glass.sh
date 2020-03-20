65bbb7bdc6c46605b3f6702eb659652c2a7a781f
Helge Reelfs <reelfs@b-tu.de>
2020-03-20 10:09:12 +0100

    adds looking glass script to IXP

---
+++ platform/docker_images/ixp/looking_glass.sh
@@ -0,0 +1,20 @@
+while true
+do
+  duration=$(shuf -i300-900 -n1)
+
+  for i in {0..$duration..30}; do
+    date "+%FT%T" > /home/looking_glass.txt
+    vtysh -c 'show ip bgp' >> /home/looking_glass.txt
+    sleep 30
+  done
+
+  # save router config
+  vtysh -c 'write'
+
+  # needed to remove weird interface names when frr starts..
+  for intf_name in $(vtysh -c 'show interface brief' | grep _c | cut -f 1 -d ' '); do
+      vtysh -c "conf t" -c "interface $intf_name" -c "shutdown" -c "exit" -c "no interface $intf_name"
+  done
+
+
+done
